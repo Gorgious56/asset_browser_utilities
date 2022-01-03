d9071cdc575f80fe95c65f46c27d095ed6afbc40
Gorgious <nathan.hild@gmail.com>
2022-01-03 12:24:07 +0100

    Add options to execute only on current file

--- ui/menu.py
+++ ui/menu.py
@@ -10,13 +10,27 @@ class BGABP_MT_menu(bpy.types.Menu):
     def poll(cls, context):
         return SpaceAssetInfo.is_asset_browser_poll(context)
 
-    def draw(self, _context):
+    def draw(self, context):
         layout = self.layout
 
-        mark_op = layout.operator("asset.batch_mark", text="Mark Assets")
-        mark_op.unmark = False
-        unmark_op = layout.operator("asset.batch_mark", text="Unmark Assets")
-        unmark_op.unmark = True
+        mark_op = layout.operator("asset.batch_mark_or_unmark", text="Mark Assets in External Library")
+        mark_op.this_file_only = False
+        mark_op.mark = True
+
+        unmark_op = layout.operator("asset.batch_mark_or_unmark", text="Unmark Assets in External Library")
+        unmark_op.this_file_only = False
+        unmark_op.mark = False
+
+        if bpy.data.is_saved:
+            mark_op = layout.operator("asset.batch_mark_or_unmark", text="Mark Assets in this File")
+            mark_op.this_file_only = True
+            mark_op.mark = True
+
+            unmark_op = layout.operator("asset.batch_mark_or_unmark", text="Unmark Assets in this File")
+            unmark_op.this_file_only = True
+            unmark_op.mark = False
+        else:
+            layout.label(text="Make sure to save this file to enable marking assets", icon="QUESTION")
 
 
 def menu_draw(self, context):
