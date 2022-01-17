31c9172534098a4ce37ffe53cb521bdea5c08365
Gorgious <nathan.hild@gmail.com>
2022-01-17 21:35:39 +0100

    Feature #23 : Batch Remove from catalog

--- catalog/remove_from_catalog.py
+++ catalog/remove_from_catalog.py
@@ -12,12 +12,12 @@ from asset_browser_utilities.catalog.helper import (
 from asset_browser_utilities.catalog.prop import FilterCatalog
 
 
-class BatchMoveToCatalog(BatchExecute):
+class BatchRemoveFromCatalog(BatchExecute):
     def execute_one_file_and_the_next_when_finished(self):
-        uuid, tree, name = get_catalog_info_from_line(self.catalog_line)
-        ensure_catalog_exists(uuid, tree, name)
+        uuid, _, _ = get_catalog_info_from_line(self.catalog_line)
         for asset in self.assets:
-            asset.asset_data.catalog_id = uuid
+            if asset.asset_data.catalog_id == uuid:
+                asset.asset_data.catalog_id = ""
         self.save_file()
         self.execute_next_blend()
 
@@ -28,16 +28,16 @@ class OperatorProperties(PropertyGroup):
 
     def draw(self, layout):
         box = layout.box()
-        box.label(text="Move Assets to This Catalog")
+        box.label(text="Remove Assets From This Catalog")
         box.prop(self.catalog, "catalog", icon="ASSET_MANAGER")
 
 
 class ASSET_OT_batch_move_to_catalog(Operator, ImportHelper, BatchOperator):
-    bl_idname = "asset.batch_move_to_catalog"
-    bl_label = "Batch Move To Catalog"
+    bl_idname = "asset.batch_remove_from_catalog"
+    bl_label = "Batch Remove From Catalog"
 
     operator_settings: PointerProperty(type=OperatorProperties)
-    logic_class = BatchMoveToCatalog
+    logic_class = BatchRemoveFromCatalog
 
     def invoke(self, context, event):
         return self._invoke(context, filter_assets=True)
