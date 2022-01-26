2153f80b7729ed7f29b0c1f45b7f2fffc1cec030
Gorgious <nathan.hild@gmail.com>
2022-01-26 08:22:03 +0100

    Improve asset preview generation check

--- preview/helper.py
+++ preview/helper.py
@@ -1,11 +1,22 @@
 import numpy as np
+import bpy
 
+def can_preview_be_generated(asset):
+    if isinstance(asset, (bpy.types.Material, bpy.types.Action)):
+        return True
+    elif isinstance(asset, bpy.types.Object):
+        if asset.type in ("MESH", "FONT", "LIGHT", "GREASEPENCIL", "VOLUME", "SURFACE", "META"):
+            if asset.type == "MESH" and len(asset.data.polygons) == 0:
+                return False
+            return True
+    return False
 
 def is_preview_generated(asset):
-    if asset.type == "MESH" and len(asset.data.polygons) == 0:
-        return True
     preview = asset.preview
-    if not preview:
+    if preview:
+        if not preview.image_pixels:
+            return True
+    else:
         asset.asset_generate_preview()
     arr = np.zeros((preview.image_size[0] * preview.image_size[1]) * 4, dtype=np.float32)
     preview.image_pixels_float.foreach_get(arr)
