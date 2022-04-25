79d741d782de1802d6e6de8ed5bbb5d87e8a052a
Gorgious <nathan.hild@gmail.com>
2022-04-25 17:01:57 +0200

    Feature #52 : Display asset data custom properties Slight problem, the user now can delete the dimensions custom prop which seems like they shouldn't be able to

---
+++ custom_property/ui/panel.py
@@ -0,0 +1,11 @@
+import bpy
+from rna_prop_ui import PropertyPanel
+
+
+class GU_PT_asset_meta_data_custom_properties(bpy.types.Panel, PropertyPanel):
+    _context_path = "id.asset_data"
+    _property_type = bpy.types.AssetMetaData
+    bl_label = "Custom Properties"
+    bl_idname = "GU_PT_asset_meta_data_custom_properties"
+    bl_space_type = "FILE_BROWSER"
+    bl_region_type = "TOOL_PROPS"
