3c82a2b9a259cae52a9ee3830921844e1b04b7ca
Gorgious <nathan.hild@gmail.com>
2023-11-08 16:03:27 +0100

    set open linked blend file outliner context menu in asset mark menu so it is more broadly available

--- core/ui/context/asset_browser.py
+++ core/ui/context/asset_browser.py
@@ -14,13 +14,11 @@ def draw_asset_browser_context_menu_appends(self, context):
 
 def register():
     bpy.types.ASSETBROWSER_MT_context_menu.append(draw_asset_browser_context_menu_appends)
-    bpy.types.OUTLINER_MT_collection.append(draw_outliner_context_menu_appends)
-    bpy.types.OUTLINER_MT_object.append(draw_outliner_context_menu_appends)
+    bpy.types.OUTLINER_MT_asset.append(draw_outliner_context_menu_appends)
     bpy.types.VIEW3D_MT_object_context_menu.append(draw_outliner_context_menu_appends)
 
 
 def unregister():
     bpy.types.ASSETBROWSER_MT_context_menu.remove(draw_asset_browser_context_menu_appends)
-    bpy.types.OUTLINER_MT_collection.remove(draw_outliner_context_menu_appends)
-    bpy.types.OUTLINER_MT_object.remove(draw_outliner_context_menu_appends)
+    bpy.types.OUTLINER_MT_asset.remove(draw_outliner_context_menu_appends)
     bpy.types.VIEW3D_MT_object_context_menu.remove(draw_outliner_context_menu_appends)
