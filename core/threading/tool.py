fdd091a1504a781a80509dd321e8e3126fcff9b0
Gorgious <nathan.hild@gmail.com>
2023-12-03 21:58:31 +0100

    Threads are now throttled in a pool to avoid freezing when libraries contain a lot of files to open. As a result the overall execution will be a little slower, but this depends on the machine specs and the size of the blend files to open

--- core/threading/tool.py
+++ core/threading/tool.py
@@ -1,10 +1,9 @@
 import threading
 
-
 from asset_browser_utilities.core.log.logger import Logger
 
-
 class ThreadManager:
+    THREAD_POOL: int = 25
     threads: list[threading.Thread]
     threads_progress: list[float]
 
@@ -16,12 +15,29 @@ class ThreadManager:
             threading.Thread(name="Thread %d" % i, target=ThreadManager.execute_function_wrapper)
             for i in range(ThreadManager.thread_count)
         ]
-        ThreadManager.callback = callback
+        ThreadManager.threads_to_run = ThreadManager.threads[:]
+        ThreadManager.running_threads = 0
+
+        def internal_callback():
+            ThreadManager.running_threads -= 1
+            ThreadManager.update_threads()
+            callback()
+
+        ThreadManager.callback = internal_callback
+
+    @staticmethod
+    def update_threads():
+        while ThreadManager.running_threads < ThreadManager.THREAD_POOL:
+            if ThreadManager.threads_to_run:
+                ThreadManager.threads_to_run.pop(0).start()
+                ThreadManager.running_threads += 1
+            else:
+                break
+            
 
     @staticmethod
     def run(wait_for_execution=True):
-        for thread in ThreadManager.threads:
-            thread.start()
+        ThreadManager.update_threads()
         if wait_for_execution:
             for thread in ThreadManager.threads:
                 thread.join()
@@ -29,5 +45,6 @@ class ThreadManager:
 
     @staticmethod
     def execute_function_wrapper():
+        # TODO : use a queue to throttle a fixed thread pool and assign it tasks. But next line won't work anymore.
         ThreadManager.execute_function(index=ThreadManager.threads.index(threading.current_thread()))
         ThreadManager.callback()
